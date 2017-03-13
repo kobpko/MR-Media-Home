f1b740b80ed48afaec79f5cdf33e9eeb0e36e0e6
kobpko <zhangjiaqi121@126.com>
2017-03-13 17:29:59 +0800

    add login and signup page. There is still a problem for file upload.

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,8 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'MyCtrl1'});
-    $routeProvider.when('/login', {templateUrl: 'partials/logIn.html', controller: 'MyCtrl2'});
+    $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'});
+    $routeProvider.when('/login', {templateUrl: 'partials/logIn.html', controller: 'LoginCtrl'});
+    $routeProvider.when('/signup', {templateUrl: 'partials/SignUp.html', controller: 'SignupCtrl'});
     $routeProvider.otherwise({redirectTo: '/home'});
   }]);
