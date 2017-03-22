17533da955bc126ed350dce619541fbfe8ba9a7e
kobpko <zhangjiaqi121@126.com>
2017-03-22 20:36:39 +0800

    优化注册界面。

--- app/js/controller/homeController.js
+++ app/js/controller/homeController.js
@@ -2,6 +2,17 @@
  * Created by MSI on 2017/3/16.
  */
 mrmedia.
-controller('HomeCtrl', [function() {
+controller('HomeCtrl', ['$scope',function($scope) {
     $('#myCarousel').carousel();
+    $scope.img = {
+        img_slide1 : 'slide-1.jpg',
+        img_slide2 : 'slide-2.jpg',
+        img_slide3 : 'slide-3.jpg',
+        add : 'add.png',
+        join : 'join.jpg',
+        work_title : 'work-title.png',
+        iphone_1 : 'iphone-bg-lg.min.png',
+        iphone_2 : 'iphone6.min.png'
+    }
+
 }])
