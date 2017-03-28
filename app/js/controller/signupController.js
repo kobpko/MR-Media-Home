264a283351b6b6b103e3a55ddafbe93b78361acb
kobpko <zhangjiaqi121@126.com>
2017-03-28 09:47:23 +0800

    sign up file.

--- app/js/controller/signupController.js
+++ app/js/controller/signupController.js
@@ -6,13 +6,13 @@ mrmedia.controller('SignupCtrl', ['$scope',function($scope) {
     // 文件上传框
     $("#uploadfile").fileinput({
         language: 'zh', //设置语言
-        uploadUrl: "../img/", //上传的地址
+        uploadUrl: "img/update.json", //上传的地址
         allowedFileExtensions: ['jpg', 'gif', 'png'],//接收的文件后缀
         //uploadExtraData:{"id": 1, "fileName":'123.mp3'},
-        uploadAsync: false, //默认异步上传
-        showUpload: true, //是否显示上传按钮
+        uploadAsync: true, //默认异步上传
+        showUpload: false, //是否显示上传按钮
         showRemove : true, //显示移除按钮
-        showPreview : true, //是否显示预览
+        showPreview : true, //是否显示预览true
         showCaption: false,//是否显示标题
         browseClass: "btn btn-primary", //按钮样式
         dropZoneEnabled: false,//是否显示拖拽区域
@@ -22,12 +22,26 @@ mrmedia.controller('SignupCtrl', ['$scope',function($scope) {
         //maxImageHeight: 1000,//图片的最大高度
         maxFileSize: 100,//单位为kb，如果为0表示不限制文件大小
         //minFileCount: 0,
-        maxFileCount: 10, //表示允许同时上传的最大文件个数
+        maxFileCount: 1, //表示允许同时上传的最大文件个数
         enctype: 'multipart/form-data',
         validateInitialCount:true,
         previewFileIcon: "<i class='glyphicon glyphicon-king'></i>",
         msgFilesTooMany: "选择上传的文件数量({n}) 超过允许的最大数值{m}！",
-    });
+    }).on("filebatchselected", function(event, files) {
+        $(this).fileinput("upload");
+    }).on("fileuploaded", function(event, data) {
+            alert('123');
+            if(data.response)
+            {
+                alert('处理成功');
+                var file = data.reader.result;
+                var filename = data.files[0].name;
+                var type = data.files[0].type;
+                var size = data.files[0].type;
+                // we can use ajax.
+            }
+        });
+
 
     // $('.avatar-input').change(function(event) {
     //     // 根据这个 <input> 获取文件的 HTML5 js 对象
@@ -49,7 +63,7 @@ mrmedia.controller('SignupCtrl', ['$scope',function($scope) {
     //         var URL = window.URL || window.webkitURL;
     //         // 通过 file 生成目标 url
     //         var imgURL = URL.createObjectURL(file);
-    //         // 使用下面这句可以在内存中释放对此 url 的伺服，跑了之后那个 URL 就无效了
+    //         // 使用下面这句可以在内存中释放对此 url 的伺服，跑s了之后那个 URL 就无效了
     //         // URL.revokeObjectURL(imgURL);
     //         $('#avatarImg').cropper('replace',imgURL);
     //         console.log(imgURL);
@@ -65,8 +79,6 @@ mrmedia.controller('SignupCtrl', ['$scope',function($scope) {
     // };
     //
     // $('#avatarImg').cropper(option);
-
-
     $scope.user= {
 
     }
@@ -77,8 +89,9 @@ mrmedia.controller('SignupCtrl', ['$scope',function($scope) {
         alert(_url);
     }
     $scope.signup = function(){
-        //we have two interface for signup. one for signup,the other for file upload.
-        alert('test');
+        //we have two interface for signup. one for signup,the other for file upload
+        alert('test:' + $scope.user);
+        var user = $scope.user;
     };
 
 }]);
